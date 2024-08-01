c8e224c4de3a35b4445890d14bfb17467017c77c
= <=>
2024-08-01 13:51:29 -0300

    corrigindo o src da imagem de loading

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import bola from "./../assets/images/bola.png"
+
 
 const Loading = () => {
   return (
     <div className='flex flex-col items-center justify-center h-screen'>
       <h1 className='text-2xl text-bold mb-3'>Carregando...</h1>
-      <img className='animate-spin' src="src/assets/images/bola.png" alt="" />
+      <img className='animate-spin' src={bola} alt="" />
     </div>
   )
 }
